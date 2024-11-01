e7430cd5e49ce422f55c987a56ba361d0641a6fc
Radek <rad.szep@gmail.com>
2024-11-01 16:20:46 +0100

    Ukończona symulacja, TO-DO testy

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -2,20 +2,23 @@ package agh.ics.oop;
 
 import agh.ics.oop.model.MoveDirection;
 import agh.ics.oop.model.Animal;
+import agh.ics.oop.model.Vector2d;
+import agh.ics.oop.Simulation;
 
 import java.util.List;
+import java.util.Vector;
 
 public class World {
     public static void main(String[] args) {
         //System.out.println("Start");
         List<MoveDirection> directions = OptionsParser.parser(args);
-        run(directions);
+        List<Vector2d> positions = List.of(new Vector2d(2,2), new Vector2d(3,4));
+        Simulation simulation = new Simulation(positions, directions);
+        simulation.run();
+        //run(directions);
         //System.out.println("Stop");
-
         Animal animal = new Animal();
         System.out.println(animal.position);
-
-
 
 
 
--- oolab/src/main/java/agh/ics/oop/model/Animal.java
+++ oolab/src/main/java/agh/ics/oop/model/Animal.java
@@ -1,5 +1,6 @@
 package agh.ics.oop.model;
 import agh.ics.oop.model.Vector2d;
+import agh.ics.oop.Simulation;
 
 public class Animal {
     private MapDirection orientation = MapDirection.NORTH;
@@ -13,8 +14,9 @@ public class Animal {
         this.position = vector;
     }
 
+    @Override
     public String toString(){
-        return "Pozycja zwierzaka (%s, %s), orientacja: %s".formatted(this.position.getX(), this.position.getY(), this.orientation);
+        return "(%s, %s)".formatted(this.position.getX(), this.position.getY());
     }
 
     public boolean isAt(Vector2d position) {
@@ -25,21 +27,31 @@ public class Animal {
         Vector2d newPosition = this.position;
         switch(direction) {
             case LEFT: this.orientation = orientation.previous();
+                break;
             case RIGHT: this.orientation = orientation.next();
+                break;
             case FORWARD:
                 switch (this.orientation) {
                     case NORTH: newPosition = new Vector2d(this.position.getX(), this.position.getY()+1);
+                        break;
                     case SOUTH: newPosition = new Vector2d(this.position.getX(), this.position.getY()-1);
+                        break;
                     case EAST: newPosition = new Vector2d(this.position.getX()+1, this.position.getY());
+                        break;
                     case WEST: newPosition = new Vector2d(this.position.getX()-1, this.position.getY());
-                };
+                        break;
+                }; break;
             case BACKWARD:
                 switch (this.orientation) {
                     case NORTH: newPosition = new Vector2d(this.position.getX(), this.position.getY()-1);
+                        break;
                     case SOUTH: newPosition = new Vector2d(this.position.getX(), this.position.getY()+1);
+                        break;
                     case EAST: newPosition = new Vector2d(this.position.getX()-1, this.position.getY());
+                        break;
                     case WEST: newPosition = new Vector2d(this.position.getX()+1, this.position.getY());
-                };
+                        break;
+                }; break;
         };
         if (position.getX() >= 0 && position.getX() <= 4 &&
                 position.getY() >= 0 && position.getY() <= 4) {
