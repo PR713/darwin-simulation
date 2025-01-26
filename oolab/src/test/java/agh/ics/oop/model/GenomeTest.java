8beeebd9d73d659d2d755df4ceff48f341e33d3e
Radek <rad.szep@gmail.com>
2025-01-26 23:51:14 +0100

    Koniec <3

--- oolab/src/test/java/agh/ics/oop/model/GenomeTest.java
+++ oolab/src/test/java/agh/ics/oop/model/GenomeTest.java
@@ -2,8 +2,7 @@ package agh.ics.oop.model;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GenomeTest {
 
@@ -36,38 +35,67 @@ public class GenomeTest {
         int[] childGenes = childGenome.getGenes();
         assertEquals(genomeLength, childGenes.length);
 
-        int mutationLeftCount = 0;
-        int genomeDivisor = (int) Math.floor((double) 40/(60+40) * (genomeLength - 1));
+        for (int gene : childGenes) {
+            assertTrue(gene >= 0 && gene < 8);
+        }
 
+        int mutationLeftCount = 0;
+        int genomeDivisor = (int) Math.floor((double) 60/(60+40) * (genomeLength - 1));
+        //left side stronger's one
         for (int i = 0; i < childGenes.length; i++) {
             if (i < genomeDivisor ) {
-                if (childGenes[i] != g1.getGenes()[i]) {
+                if (childGenes[i] != g2.getGenes()[i]) {
                     mutationLeftCount++;
                 }
             } else {
-                if (childGenes[i] != g2.getGenes()[i]) {
+                if (childGenes[i] != g1.getGenes()[i]) {
                     mutationLeftCount++;
                 }
             }
         }
+
 
         int mutationRightCount = 0;
+        //right side stronger's one
         for (int i = 0; i < childGenes.length; i++) {
             if (i < genomeDivisor ) {
-                if (childGenes[i] != g2.getGenes()[i]) {
+                if (childGenes[i] != g1.getGenes()[i]) {
                     mutationRightCount++;
                 }
             } else {
-                if (childGenes[i] != g1.getGenes()[i]) {
+                if (childGenes[i] != g2.getGenes()[i]) {
                     mutationRightCount++;
                 }
             }
         }
 
-        System.out.println(mutationCount);
-        System.out.println(childGenome);
-        System.out.println(g1);
-        System.out.println(g2);
+        int mutationCount = Math.min(mutationLeftCount, mutationRightCount);
+
         assertTrue(mutationCount >= 0 && mutationCount <= maxMutations);
+    }
+
+
+    @Test
+    public void testEquals(){
+        Genome genome1 = new Genome(50);
+        Genome genome2 = new Genome(50); //szansa że takie same 1/8^50...
+
+        assertTrue(genome1.equals(genome1));
+        assertFalse(genome1.equals(genome2));
+        assertFalse(genome1.equals(null));
+    }
+
+
+    @Test
+    public void testToString(){
+        Genome genome = new Genome(10);
+        int[] genes = genome.getGenes();
+        String genomeString = genome.toString();
+
+        assertEquals(10, genomeString.length());
+
+        for (int i = 0; i < 10; i++) {
+            assertEquals(Character.getNumericValue(genomeString.charAt(i)), genes[i]);
+        }
     }
 }
